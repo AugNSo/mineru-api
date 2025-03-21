d27858f15500b34285201ca096386e23218ba3e9
AugNSo <song.tiankai@icloud.com>
2025-03-21 18:10:22 +0800

    fix start script

--- start.sh
+++ start.sh
@@ -5,8 +5,8 @@ rm -f output/images/*
 
 # shutdown any running processes
 python worker.py shutdown
-python app.py shutdown
+nohup python app.py shutdown >> /dev/null 2>&1 &
 
 # start the worker and server
 nohup python worker.py work > rq.log 2>&1 &
-nohup python app.py > server.log 2>&1 &
+nohup python app.py start > server.log 2>&1 &
