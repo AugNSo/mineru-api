f5dfafa6b3b2a43ad41f93d67f7a7485406d7191
AugNSo <song.tiankai@icloud.com>
2025-03-21 16:12:50 +0800

    update start script

--- start.sh
+++ start.sh
@@ -1,5 +1,12 @@
 # !/bin/bash
+
+# clean up temp files
 rm -f output/images/*
+
+# shutdown any running processes
 python worker.py shutdown
+python app.py shutdown
+
+# start the worker and server
 nohup python worker.py work > rq.log 2>&1 &
 nohup python app.py > server.log 2>&1 &
