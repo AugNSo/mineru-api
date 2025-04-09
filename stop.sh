e9aa6d4a7c56c1bace8f06706043a8596caef547
AugNSo <song.tiankai@icloud.com>
2025-04-10 00:13:11 +0800

    minor update of stop script

--- stop.sh
+++ stop.sh
@@ -2,5 +2,5 @@
 # clean up temp files
 rm -f output/images/*
 # shutdown any running processes
-python worker.py shutdown
+python worker.py shutdown >> /dev/null 2>&1 &
 curl -X POST 'http://localhost:9721/shutdown' >> /dev/null 2>&1 &
