68e92eb9483e4f1d47ea6bb0b3d50a1ac7622853
AugNSo <song.tiankai@icloud.com>
2025-04-11 10:28:47 +0800

    update download_model_hf.py

---
+++ update_download_model_py.sh
@@ -0,0 +1,2 @@
+wget https://github.com/opendatalab/MinerU/raw/refs/heads/master/scripts/download_models.py -O download_models.py
+wget https://github.com/opendatalab/MinerU/raw/refs/heads/master/scripts/download_models_hf.py -O download_models_hf.py
