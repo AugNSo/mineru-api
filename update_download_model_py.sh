c559cef4d3ef2be695da883edf4b38126e99f9b9
AugNSo <song.tiankai@icloud.com>
2025-04-17 16:05:03 +0800

    skip certificate check when updating download_model scripts

--- update_download_model_py.sh
+++ update_download_model_py.sh
@@ -1,2 +1,2 @@
-wget https://github.com/opendatalab/MinerU/raw/refs/heads/master/scripts/download_models.py -O download_models.py
-wget https://github.com/opendatalab/MinerU/raw/refs/heads/master/scripts/download_models_hf.py -O download_models_hf.py
+wget --no-check-certificate https://github.com/opendatalab/MinerU/raw/refs/heads/master/scripts/download_models.py -O download_models.py
+wget --no-check-certificate https://github.com/opendatalab/MinerU/raw/refs/heads/master/scripts/download_models_hf.py -O download_models_hf.py
